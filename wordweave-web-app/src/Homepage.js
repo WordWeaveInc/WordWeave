636b1da11231fe21d23afbada5c1c06cb6a1da67
tylerhilton3 <tyler.hilton@sbcglobal.net>
2024-11-21 11:13:09 -0700

    started routing

---
+++ wordweave-web-app/src/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Homepage() {
+  return (
+    <div>
+      <h1>Welcome to the Homepage!</h1>
+      <p>If you see this, the routing works!</p>
+    </div>
+  );
+}
+
+export default Homepage;
--- wordweave-web-app/src/index.js
+++ wordweave-web-app/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'; 
 import './styles/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Homepage from './Homepage';
+import About from './About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
